ee009baaa446fec84e5750acfabe002a8dc795c8
josepbw82 <joseph.wade82@gmail.com>
2017-11-06 03:00:25 -0500

    Update and rename arch to arch_cfg.h

---
+++ arch_cfg.h
@@ -0,0 +1,17 @@
+#ifndef _arch_h
+#define _arch_h
+
+#ifdef _x86_64
+# include "/arch/x86_64.h"
+#else
+# include "/arch/x86_32.h"
+#endif
+
+#ifdef (_x86_64_brand_amd == 1)&(_x86_64_brand_intel == 0)
+# include "/arch/x86_64/amd.h"
+#
+#ifdef (_x86_64_brand_amd == 0)&(_x86_64_intel == 1)
+# include "/arch/x86_64/intel.h"
+#else
+# return ERR_MSG(ERNOARCHSUP)
+#endif
