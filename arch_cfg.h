e6b3928c56472b038a2c2fff40983fecc854bf43
josepbw82 <joseph.wade82@gmail.com>
2017-11-06 03:04:19 -0500

    Update arch_cfg.h

--- arch_cfg.h
+++ arch_cfg.h
@@ -8,10 +8,10 @@
 #endif
 
 #ifdef (_x86_64_brand_amd == 1)&(_x86_64_brand_intel == 0)
-# include "/arch/x86_64/amd.h"
-#
-#ifdef (_x86_64_brand_amd == 0)&(_x86_64_intel == 1)
-# include "/arch/x86_64/intel.h"
+# include "/arch/x86_64/amd.h"    /* amd_subarch_cfg */
 #else
-# return ERR_MSG(ERNOARCHSUP)
+#ifdef (_x86_64_brand_amd == 0)&(_x86_64_intel == 1)
+# include "/arch/x86_64/intel.h"  /*intel_subarch_cfg */
+#else
+# return ERR_MSG(ERNOARCHSUPPORT)  /* Error, ARCH not supported */
 #endif
